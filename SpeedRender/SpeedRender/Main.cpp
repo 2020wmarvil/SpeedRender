4b5fdc1df98945eaf0e06162fa3f4449ff363982
Wyatt Marvil <2020wmarvil@gmail.com>
2022-03-29 11:58:50 -0400

    VAO,VBO,EBO

--- SpeedRender/SpeedRender/Main.cpp
+++ SpeedRender/SpeedRender/Main.cpp
@@ -1,4 +1,4 @@
-#define GLFW_INCLUDE_NONE
+﻿#define GLFW_INCLUDE_NONE
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
@@ -7,194 +7,156 @@
 
 #include <iostream>
 
-#define WIDTH 640
-#define HEIGHT 480
+#define WIDTH 960
+#define HEIGHT 540
 
-float vertices[] = {
-    -0.5f, -0.5f, 0.5f,
-     0.5f, -0.5f, 0.5f,
-    -0.5f, -0.5f, -0.5f,
-     0.5f, -0.5f, -0.5f,
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);  
+void processInput(GLFWwindow* window);
 
-    -0.5f, 0.5f, 0.5f,
-     0.5f, 0.5f, 0.5f,
-    -0.5f, 0.5f, -0.5f,
-     0.5f, 0.5f, -0.5f,
-}; 
+int main() {
+    // initialization
+	glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-int indices[] = {
-    0, 3, 2,
-    0, 1, 3,
-
-    1, 8, 3,
-    1, 6, 8,
-
-    6, 7, 8,
-    6, 5, 7,
-
-    5, 0, 2,
-    5, 2, 7,
-
-    5, 6, 1,
-    5, 1, 0,
-
-    7, 8, 3,
-    7, 3, 2,
-};
-
-static const char* vertex_shader_text =
-"#version 330 core\n"
-"layout(location = 0) in vec3 position;\n"
-"uniform mat4 mvp;\n"
-"void main() {\n"
-"    gl_Position = mvp * vec4(position, 1.0);\n"
-"}\n";
- 
-static const char* fragment_shader_text =
-"#version 110\n"
-"void main()\n"
-"{\n"
-"    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n"
-"}\n";
-
-glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 0.0f);
-glm::mat4 proj;
-
-void error_callback(int error, const char* description) {
-    fprintf(stderr, "Error: %s\n", description);
-}
-
-struct Params {
-    uint16_t state;
-
-    uint16_t W_DOWN = 1 << 0;
-    uint16_t A_DOWN = 1 << 1;
-    uint16_t S_DOWN = 1 << 2;
-    uint16_t D_DOWN = 1 << 3;
-};
-
-Params params;
-
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-    std::cout << "key action ";
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
-    } else if (key == GLFW_KEY_A) {
-        if (action == GLFW_PRESS) { params.state |= params.A_DOWN; }
-        else if (action == GLFW_RELEASE) params.state &= ~params.A_DOWN;
-    } else if (key == GLFW_KEY_D) {
-        if (action == GLFW_PRESS) { params.state |= params.D_DOWN; }
-        else if (action == GLFW_RELEASE) params.state &= ~params.D_DOWN;
-    } else if (key == GLFW_KEY_S) {
-        if (action == GLFW_PRESS) { params.state |= params.S_DOWN; }
-        else if (action == GLFW_RELEASE) params.state &= ~params.S_DOWN;
-    } else if (key == GLFW_KEY_W) {
-        if (action == GLFW_PRESS) { params.state |= params.W_DOWN; }
-        else if (action == GLFW_RELEASE) params.state &= ~params.W_DOWN;
+    // window creation
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Speed Render", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
     }
-    std::cout << params.state << std::endl;
-}
+    glfwMakeContextCurrent(window);
 
-int main(void) {
-	if (!glfwInit()) {
-		std::cout << "GLFW Init failed" << std::endl;
-	}
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }   
 
-	glfwSetErrorCallback(error_callback);
+    glViewport(0, 0, WIDTH, HEIGHT);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
 
-	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "My Title", NULL, NULL);
-	if (!window) {
-		std::cout << "Window creation failed" << std::endl;
-	}
-	glfwMakeContextCurrent(window);
-    gladLoadGL();
-    glfwSwapInterval(1);
+    // vertex shader
+    const char *vertexShaderSource = "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "layout (location = 1) in vec3 aColor;\n"
+        "out vec3 vertexColor;\n"
+        "void main() {\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "   vertexColor = aColor;\n"
+        "}\0";
 
-	glfwSetKeyCallback(window, key_callback);
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
 
-	int width, height;
-	glfwGetFramebufferSize(window, &width, &height);
-	glViewport(0, 0, width, height);
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
 
-	GLuint vertex_buffer, vertex_shader, fragment_shader, program;
-    GLint mvp_location, vpos_location, vcol_location;
+    // fragment shader
+    const char* fragmentShaderSource = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "in vec3 vertexColor;\n"
+        "void main() {\n"
+        "   FragColor = vec4(vertexColor, 1.0);\n"
+        "}\n";
 
-	glGenBuffers(1, &vertex_buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    success = 0;
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if(!success) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // shader program
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    success = 0;
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::LINK_FAILED\n" << infoLog << std::endl;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // mesh data
+    float vertices[] = {
+        // positions         // colors
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+    };  
+    unsigned int indices[] = {
+        0, 1, 3
+    }; 
+
+    // set up buffers
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);  
+    glBindVertexArray(VAO);
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
- 
-    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
-    glCompileShader(vertex_shader);
- 
-    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
-    glCompileShader(fragment_shader);
- 
-    program = glCreateProgram();
-    glAttachShader(program, vertex_shader);
-    glAttachShader(program, fragment_shader);
-    glLinkProgram(program);
- 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);  
 
-    proj = glm::perspective(45.0f, 800.0f / 600.0f, 1.0f, 10.0f);
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+    glEnableVertexAttribArray(1);
 
-    glm::mat4 view = glm::lookAt(
-        cameraPos,
-        glm::vec3(0.0f, 0.0f, 0.0f),
-        glm::vec3(0.0f, 1.0f, 0.0f));
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
 
-    float lastTime = glfwGetTime();
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
+    // render loop
+    while(!glfwWindowShouldClose(window)) {
+        processInput(window);
 
-        float currentTime = glfwGetTime();
-        float deltaTime = currentTime - lastTime;
-        lastTime = currentTime;
-
-        float moveAmount = 3.0f;
-
-        glm::vec3 movement(0.0f);
-
-        if (params.state & params.W_DOWN) {
-            movement.x += moveAmount * deltaTime;
-        } else if (params.state & params.S_DOWN) {
-            movement.x -= moveAmount * deltaTime;
-        }
-        if (params.state & params.A_DOWN) {
-            movement.y -= moveAmount * deltaTime;
-        } else if (params.state & params.D_DOWN) {
-            movement.y += moveAmount * deltaTime;
-        }
-
-        glViewport(0, 0, width, height);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        //Set up MVP
-        glm::mat4 model(1.0f);
-        //model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-        view = glm::translate(view, movement);
-        glm::mat4 mvp = proj * view * model;
-
-        glUseProgram(program);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        
-        GLint uniMvp = glGetUniformLocation(program, "mvp");
-        glUniformMatrix4fv(uniMvp, 1, GL_FALSE, glm::value_ptr(mvp));
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
+        glfwPollEvents();    
+    }
 
-        GLenum err;
-        while((err = glGetError()) != GL_NO_ERROR) {
-            std::cout << err << std::endl;
-        }
-	}
+    // cleanup
+    glfwTerminate();
 
-	glfwDestroyWindow(window);
-	glfwTerminate();
-
+    return 0;
 }
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}  
 
+void processInput(GLFWwindow *window) {
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
